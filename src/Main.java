126dd226bfcdc27c3f4ad8d75bd1edb194b6552d
Soprodevento <Sashinka2803@gmail.com>
2024-04-10 23:39:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isWeekend("Monday"));
+        String[] days =  {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        System.out.println(weekendCount(days));
+        System.out.println(weekdayCount(days));
+    }
+
+    public static boolean isWeekend(String dayName) {
+        switch (dayName) {
+            case "Saturday":
+                return true;
+            case "Sunday":
+                return true;
+            default:
+                return false;
+        }
+    }
+
+
+    public static int weekendCount(String[] days) {
+        int count = 0;
+        for(String day : days){
+            if( day.equals("Saturday")|| day.equals("Sunday") ){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static int weekdayCount(String[] days) {
+        int count = 0;
+        for(String day : days){
+            if (!day.equals("Saturday")&&!day.equals("Sunday")){
+                count++;
+            }
+        }
+        return count;
+    }
+}
